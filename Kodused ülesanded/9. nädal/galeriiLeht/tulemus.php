c312577219e8a618ce75fb509943570ff718aecc
mailliam <maila.keerus@gmail.com>
2016-04-17 23:03:15 +0300

    9. nadal valmis

--- Kodused ülesanded/9. nädal/galeriiLeht/tulemus.php
+++ Kodused ülesanded/9. nädal/galeriiLeht/tulemus.php
@@ -1,9 +1,17 @@
 <h3>Valiku tulemus</h3>
+<P>
+    <?php if (!empty($_POST["pilt"])) {
+        if(in_array(isset($pildid[$_POST["pilt"]-1]), $pildid)) {
 
-<?php if (!empty($_POST["pilt"])) {
-	echo "Pilt valitud.";
-} else {
-    echo "Sa ei valinud ühtegi pilti";
-}?>
+            echo "Valik tehtud. Sinu valik oli pilt nr. ".$_POST["pilt"].".";?>
+            <br/>
+            <img src="<?php echo $pildid[$_POST["pilt"]-1]["src"];?>" alt="<?php echo $pildid[$_POST["pilt"]-1]["alt"] ?>" height="100"/>
+            <?php
+        } else {
+            echo "Sellist pilti ei ole valikus.";
+        }
 
-<p>Siia tuleb valiku tulemus, mida saab kuvada ainult PHP abil :)</p>
+    } else {
+        echo "Sa ei valinud ühtegi pilti.";
+    }?>
+</p>
